d575ff8b41bb01d74582c2cb31b21a1c25b702fe
Trent Apple <trentapple@users.noreply.github.com>
2016-10-17 17:04:12 -0500

    Update InstallDependencies.bat
    Added portion of script to handle UAC

--- InstallDependencies.bat
+++ InstallDependencies.bat
@@ -1,12 +1,56 @@
+:: ### START UAC SCRIPT ###
+
+if "%2"=="firstrun" exit
+cmd /c "%0" null firstrun
+
+if "%1"=="skipuac" goto skipuacstart
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (shift & goto gotPrivileges)
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%temp%\OEgetPrivileges.vbs"
+exit /B
+
+:gotPrivileges
+
+setlocal & pushd .
+
+cd /d %~dp0
+cmd /c "%0" skipuac firstrun
+cd /d %~dp0
+
+:skipuacstart
+
+if "%2"=="firstrun" exit
+
+:: ### END UAC SCRIPT ###
+
+:: ### START OF YOUR OWN BATCH SCRIPT BELOW THIS LINE ###
+
+@echo off
+
 REM Install Chocolatey:
 echo ## Step 1: ##
+
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Install Curl and 7-Zip:
 echo ## Step 2: ##
+
 choco install -y curl 7zip
 
+REM README
 echo ## Note: ##
+
 echo ( Please note this script must be run as local administrator -- 
 echo there is a non-administrator setup, but is generally recommended as a last resort. 
 echo Read more: https://chocolatey.org/install#non-administrative-install )
