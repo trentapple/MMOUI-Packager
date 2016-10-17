7b9be7b2fd717224e4999cc367843d28ba42540e
Trent Apple <trentapple@users.noreply.github.com>
2016-10-17 16:54:37 -0500

    Create InstallDependencies.bat

---
+++ InstallDependencies.bat
@@ -0,0 +1,5 @@
+REM Install Chocolatey:
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+REM Install Curl and 7-Zip:
+choco install -y curl 7zip
