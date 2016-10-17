4ab0ec613632c108db9eb7cf01d6a89f507a2be7
Trent Apple <trentapple@users.noreply.github.com>
2016-10-17 17:02:28 -0500

    Update InstallDependencies.bat

--- InstallDependencies.bat
+++ InstallDependencies.bat
@@ -1,5 +1,14 @@
 REM Install Chocolatey:
+echo ## Step 1: ##
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Install Curl and 7-Zip:
+echo ## Step 2: ##
 choco install -y curl 7zip
+
+echo ## Note: ##
+echo ( Please note this script must be run as local administrator -- 
+echo there is a non-administrator setup, but is generally recommended as a last resort. 
+echo Read more: https://chocolatey.org/install#non-administrative-install )
+
+echo ## Complete! ##
